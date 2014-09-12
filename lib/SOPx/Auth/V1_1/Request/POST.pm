1c7e4796f37559f7a45bf9a2a01b68269bbc3b4a
yowcowvg <yoko_ohyama@voyagegroup.com>
2014-09-12 10:38:30 +0900

    sanitized

--- lib/SOPx/Auth/V1_1/Request/POST.pm
+++ lib/SOPx/Auth/V1_1/Request/POST.pm
@@ -8,8 +8,7 @@ use SOPx::Auth::V1_1::Util qw(create_signature);
 sub create_request {
     my ($class, $uri, $params, $app_secret) = @_;
 
-    Carp::croak('Missing required parameter: time')
-            if not $params->{time};
+    Carp::croak('Missing required parameter: time') if not $params->{time};
     Carp::croak('Missing app_secret') if not $app_secret;
 
     POST $uri => {
